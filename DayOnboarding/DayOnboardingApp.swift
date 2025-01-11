713e7b4e3a8c6f56d7486c5ee30e86b2983c3e49
Denis Savishchev <denis.savishchev@gmail.com>
2025-01-11 12:33:48 +0100

    Initial Commit

---
+++ DayOnboarding/DayOnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayOnboardingApp.swift
+//  DayOnboarding
+//
+//  Created by Devis on 11/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct DayOnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
