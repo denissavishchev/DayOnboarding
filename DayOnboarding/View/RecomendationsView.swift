fccc933809a79d6396d84cadeb9d78d6bffb860c
Denis Savishchev <denis.savishchev@gmail.com>
2025-03-03 17:25:26 +0100

    recomendations descriptions

--- DayOnboarding/View/RecomendationsView.swift
+++ DayOnboarding/View/RecomendationsView.swift
@@ -10,9 +10,24 @@ struct RecomendationsView: View {
             BackButton()
             ScrollView(.vertical, showsIndicators: false){
                 VStack{
-                    ForEach(1...10, id: \.self){index in
-                            RoundedRectangle(cornerRadius: 12)
-                            .frame(width: UIScreen.main.bounds.width - 36, height: selectedIndex == index ? 200 : 100)
+                    ForEach(Array(vm.recomendations.enumerated()), id: \.element.id){index, rec in
+                        VStack{
+                            Text(rec.name)
+                                .font(.system(size: 18, weight: .bold, design: .rounded))
+                                .foregroundColor(.kGrayLight)
+                            if selectedIndex == index{
+                                RoundedRectangle(cornerRadius: 2)
+                                    .foregroundColor(.white)
+                                    .frame(width: 100, height: 2)
+                                Text(rec.info)
+                                    .font(.system(size: 14, weight: .bold, design: .rounded))
+                                    .foregroundColor(.kGrayLight)
+                            }
+                        }
+                        .padding(18)
+                        .frame(width: UIScreen.main.bounds.width - 36, height: selectedIndex == index ? nil : 50)
+                        .background(.kBlue)
+                        .clipShape(RoundedRectangle(cornerRadius: 12))
                             .onTapGesture {
                                 withAnimation{
                                     if selectedIndex == index {
